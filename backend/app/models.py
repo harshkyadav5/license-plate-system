a39ab291910c4b05e369c3b361d2d614156492ff
harshkyadav5 <harshkyadav5@gmail.com>
2026-02-04 23:05:18 +0530

    Extend Parking log to support entry/exit tracking

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, TIMESTAMP
+from sqlalchemy import (Column, Integer, String, Float, Boolean, TIMESTAMP)
 from sqlalchemy.sql import func
 from .database import Base
 
@@ -6,9 +6,22 @@ class ParkingLog(Base):
     __tablename__ = "parking_logs"
 
     id = Column(Integer, primary_key=True, index=True)
-    image_path = Column(String)
-    predicted_plate = Column(String(32))
-    actual_plate = Column(String(32))
+
+    image_path = Column(String, nullable=False)
+    crop_path = Column(String, nullable=True)
+
+    predicted_plate = Column(String(32), index=True, nullable=False)
+    actual_plate = Column(String(32), nullable=True)
     confidence = Column(Float)
+
+    status = Column(String(3), default="IN")
     entry_time = Column(TIMESTAMP, server_default=func.now())
+    exit_time = Column(TIMESTAMP, nullable=True)
+
     is_edited = Column(Boolean, default=False)
+
+    updated_at = Column(
+        TIMESTAMP,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
